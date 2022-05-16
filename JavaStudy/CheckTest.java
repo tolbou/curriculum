9a99016e2593d9fd7bbae9cb88edfcf34c41ad0d
tolbou <ttorubou@gmail.com>
2022-05-16 19:45:53 +0900

    l

---
+++ JavaStudy/CheckTest.java
@@ -0,0 +1,34 @@
+/**
+ * バブルソート チェックテスト-Java1章
+ *
+ */
+public class CheckTest {
+	public static void main(String[] args) {
+		/*
+		 * 問1 int型の配列dataを作成し、値を3,1,2で初期化しなさい
+		 */
+		int[] data = { 3, 1, 2, 5, 7 };
+		/*
+		 * 問2 以下のfor文を完成させなさい
+		 */
+		for (int i = 0; i < 5; i++) {
+			System.out.print(data[i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < 5; i++) {
+			for (int j = data.length - 1; j > i; j--) {
+				/*
+				 * 問3 以下、配列の添字を入れてソートを完成させなさい
+				 */
+				if (data[j - 1] > data[j]) {
+					int box = data[j - 1];
+					data[j - 1] = data[j];
+					data[j] = box;
+				}
+			}
+		}
+		for (int i = 0; i < data.length; i++) {
+			System.out.print(data[i] + " ");
+		}
+	}
+}
---
+++ JavaStudy/chapter1/src/chapter1/Ex1.java
@@ -0,0 +1,19 @@
+package chapter1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		List<String> list = new ArrayList<String>();
+		{
+
+			list.add("Hello Java");
+			System.out.println("hello java");
+		}
+
+	}
+
+}
