01beec169b242aeb0dc729d126a37957672af531
tolbou <ttorubou@gmail.com>
2022-05-17 17:39:09 +0900

    d

---
+++ JavaStudy/test/src/test/Test1.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Test1 {
+
+	//メインメソッド
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		sayHello();
+		
+	}
+	
+	
+	//sayHelloメソッド
+	public static void sayHello() {
+		System.out.println("Hello");
+		
+	}
+
+}
