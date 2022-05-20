789a63bd511b7701e8ba376aedbfa973ca3cbed2
tolbou <ttorubou@gmail.com>
2022-05-20 13:08:32 +0900

    2-2課題User.java(修正)

--- JavaStudy/2-2/src/study/User.java
+++ JavaStudy/2-2/src/study/User.java
@@ -20,7 +20,7 @@ class User {
 	// ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
 	// アクセス修飾子:「protected」
 	// メソッド名:「printAccountInfo」
-	protected void printAccountInfo(String userName, int id, String password) {
+	protected void printAccountInfo() {
 		System.out.println("ユーザー名は、" + this.userName);
 		System.out.println("IDは、" + this.id);
 		System.out.println("パスワードは、" + this.password);
