02d74a36243d80ef24fb80cc699c9e2a77b11907
tolbou <ttorubou@gmail.com>
2022-05-24 15:32:30 +0900

    2章チェックテスト.java

--- JavaStudy/Check2/src/constants/Constants.java
+++ JavaStudy/Check2/src/constants/Constants.java
@@ -2,7 +2,7 @@ package constants;
 
 public class Constants {
 
-    //定数（名前）
+    //定数（名前）a
     public static final String CHECK_CLASS_JAVA = "java吉";
     public static final String CHECK_CLASS_HOGE = "hoge";
     public static final String CHECK_CLASS_R2D2 = "R2D2";
